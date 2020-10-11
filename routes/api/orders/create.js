ff5b0c10ebdf6b931258266e54cb2d033a50ea9e
Talisson Ferreira <talissonf@gmail.com>
2020-10-11 10:26:57 -0300

    fix(orders/create.js): ignore storeSecret

--- routes/api/orders/create.js
+++ routes/api/orders/create.js
@@ -1,16 +1,9 @@
 const parseStatus = require('./../../../lib/ecomplus/parse-orders-status')
 
-module.exports = ({ appSdk, ecomClient, mysql }) => {
+module.exports = ({ appSdk, ecomClient, mysql, logger }) => {
   return (req, res) => {
     const body = req.body
     const storeId = req.storeId
-
-    if (!storeId || !sessionToken) {
-      return res.status(406).send({
-        status: 406,
-        message: 'Store id ou token inválidos, acesse essa página via admin https://admin.e-com.plus'
-      })
-    }
 
     if (!body ||
       !Array.isArray(body) ||
@@ -21,7 +14,7 @@ module.exports = ({ appSdk, ecomClient, mysql }) => {
       })
     }
 
-    return appSdk.getAuth(storeId).then(({ auth }) => {
+    return appSdk.getAuth(storeId).then(auth => {
       const promises = []
       for (let i = 0; i < body.length; i++) {
         const promise = mysql.fetchOrders(body[i], storeId).then(async row => {
@@ -33,8 +26,8 @@ module.exports = ({ appSdk, ecomClient, mysql }) => {
             return ecomClient.store({
               url: `orders/${body[i]}.json`,
               storeId,
-              authenticationId: auth.myId,
-              accessToken: auth.accessToken
+              authenticationId: auth.row.authentication_id,
+              accessToken: auth.row.access_token
             }).then(({ data }) => {
               const financialStatus = data.financial_status ? data.financial_status.current : null
               return mysql.insertOrders(storeId, data._id, financialStatus, parseStatus(financialStatus), data.number, null, 'ecom')
@@ -57,7 +50,7 @@ module.exports = ({ appSdk, ecomClient, mysql }) => {
     }).then(resp => {
       return res.send(resp)
     }).catch(err => {
-      console.log(err)
+      logger.error('ManualSyncErr', err)
       const { response, message } = err
       const status = response && response.status || 500
       return res.status(status).send({
